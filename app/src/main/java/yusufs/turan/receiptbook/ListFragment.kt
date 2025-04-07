636d5c2a4f4bf3992b9fb3532549274a46083e95
Yusuf S. Turan <siryusufsus@gmail.com>
2025-04-07 14:20:23 +0300

    View Binding added

--- app/src/main/java/yusufs/turan/receiptbook/ListFragment.kt
+++ app/src/main/java/yusufs/turan/receiptbook/ListFragment.kt
@@ -5,9 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import yusufs.turan.receiptbook.databinding.FragmentListBinding
+import yusufs.turan.receiptbook.databinding.FragmentReceiptBinding
 
 
 class ListFragment : Fragment() {
+
+    private var _binding: FragmentListBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,12 +23,18 @@ class ListFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false)
+        _binding = FragmentListBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
