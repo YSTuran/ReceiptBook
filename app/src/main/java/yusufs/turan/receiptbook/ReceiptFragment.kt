406474d1a432844e9948b6054b3d5458bef60ba3
Yusuf S. Turan <siryusufsus@gmail.com>
2025-04-24 15:30:10 +0300

    Permissions and activity launcher added

--- app/src/main/java/yusufs/turan/receiptbook/ReceiptFragment.kt
+++ app/src/main/java/yusufs/turan/receiptbook/ReceiptFragment.kt
@@ -1,22 +1,43 @@
 package yusufs.turan.receiptbook
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
+import android.provider.MediaStore.Images
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import yusufs.turan.receiptbook.databinding.FragmentListBinding
+import android.widget.Toast
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.material.snackbar.Snackbar
 import yusufs.turan.receiptbook.databinding.FragmentReceiptBinding
+import java.io.IOException
 
 
 class ReceiptFragment : Fragment() {
 
     private var _binding: FragmentReceiptBinding? = null
     private val binding get() = _binding!!
+    private lateinit var permissionLauncher : ActivityResultLauncher<String>
+    private lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
+    private var selectedImage : Uri?=null
+    private var selectedBitmap : Bitmap?=null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        registerlauncher()
     }
 
     override fun onCreateView(
@@ -59,7 +80,85 @@ class ReceiptFragment : Fragment() {
 
     }
     fun selectImage(view: View){
+      if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU)
+      {
+          if(ContextCompat.checkSelfPermission(requireContext(),Manifest.permission.READ_MEDIA_IMAGES) != PackageManager.PERMISSION_GRANTED)
+          {
+              if(ActivityCompat.shouldShowRequestPermissionRationale(requireActivity(),Manifest.permission.READ_MEDIA_IMAGES)){
+                  Snackbar.make(view,"You need to access the gallery and select an image.", Snackbar.LENGTH_INDEFINITE).setAction(
+                      "Allow", View.OnClickListener { permissionLauncher.launch(Manifest.permission.READ_MEDIA_IMAGES) }
+                  ).show()
+              }
+              else{
+                  permissionLauncher.launch(Manifest.permission.READ_MEDIA_IMAGES)
+              }
+          }
+          else{
+              val intentToGallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+              activityResultLauncher.launch(intentToGallery)
+          }
+      }
+      else
+      {
+          if(ContextCompat.checkSelfPermission(requireContext(),Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
+          {
+              if(ActivityCompat.shouldShowRequestPermissionRationale(requireActivity(),Manifest.permission.READ_EXTERNAL_STORAGE)){
+                  Snackbar.make(view,"You need to access the gallery and select an image.", Snackbar.LENGTH_INDEFINITE).setAction(
+                      "Allow", View.OnClickListener { permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE) }
+                  ).show()
+              }
+              else{
+                  permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+              }
+          }
+          else{
+              val intentToGallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+              activityResultLauncher.launch(intentToGallery)
+          }
+      }
+    }
 
+    fun registerlauncher(){
+
+        activityResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){result->
+           if (result.resultCode == AppCompatActivity.RESULT_OK){
+              val intentFromResult = result.data
+               if(intentFromResult != null){
+                   selectedImage = intentFromResult.data
+
+                try
+                {
+                   if(Build.VERSION.SDK_INT >= 28)
+                   {
+                       val source = ImageDecoder.createSource(requireActivity().contentResolver, selectedImage!!)
+                       selectedBitmap = ImageDecoder.decodeBitmap(source)
+                       binding.imageView.setImageBitmap(selectedBitmap)
+                   }
+                   else
+                   {
+                       selectedBitmap = MediaStore.Images.Media.getBitmap(requireActivity().contentResolver, selectedImage)
+                       binding.imageView.setImageBitmap(selectedBitmap)
+                   }
+
+                }
+                catch (e: IOException)
+                {
+                   println(e.localizedMessage)
+                }
+               }
+
+           }
+        }
+
+       permissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()){ result->
+           if(result){
+               val intentToGallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+               activityResultLauncher.launch(intentToGallery)
+           }
+           else{
+               Toast.makeText(requireContext(),"Permission Denied!", Toast.LENGTH_LONG).show()
+           }
+       }
     }
 
     override fun onDestroyView() {
